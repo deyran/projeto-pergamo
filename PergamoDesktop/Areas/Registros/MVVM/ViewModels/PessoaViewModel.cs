bd928acdc90e32570873e78c5ce99c26cdd2432f
Deyvid Rannyere <deyran@gmail.com>
2024-06-24 19:54:34 -0300

    PessoaService Update

---
+++ PergamoDesktop/Areas/Registros/MVVM/ViewModels/PessoaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PergamoDesktop.Areas.Registros.MVVM.Models;
+using PergamoDesktop.Areas.Registros.Services;
+using System.Windows.Input;
+
+namespace PergamoDesktop.Areas.Registros.MVVM.ViewModels
+{
+    public partial class PessoaViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private List<Pessoa> _pessoas;
+
+        [ObservableProperty]
+        private Pessoa _pessoaAtual;
+
+        public ICommand AddCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand DisplayCommand { get; set; }
+
+        public PessoaViewModel(IPessoaService pessoaRepository)
+        {
+            _pessoaAtual = new Pessoa();
+        }
+    }
+}
---
+++ PergamoDesktop/Areas/Registros/Services/PessoaService.cs
@@ -0,0 +1,54 @@
+﻿using PergamoDesktop.Areas.Registros.MVVM.Models;
+using SQLite;
+
+namespace PergamoDesktop.Areas.Registros.Services
+{
+    public class PessoaService : IPessoaService
+    {
+        private SQLiteAsyncConnection? _dbConnection;
+
+        public async Task InitializeAsync()
+        {
+            await SetUpDb();
+        }
+
+        private async Task SetUpDb()
+        {
+            if (_dbConnection == null)
+            {
+                string dbPath = Path.Combine("..", "..", "Database", "dbPergamo.db");
+
+                Console.WriteLine("\n---------\n" + dbPath + "\n---------\n");
+
+                _dbConnection = new SQLiteAsyncConnection(dbPath);
+                await _dbConnection.CreateTableAsync<Pessoa>();
+            }
+        }
+
+
+        public async Task<int> AddPessoa(Pessoa pessoa)
+        {
+            return await _dbConnection.InsertAsync(pessoa);
+        }
+
+        public async Task<int> DeletePessoa(Pessoa pessoa)
+        {
+            return await _dbConnection.DeleteAsync(pessoa);
+        }
+
+        public async Task<Pessoa> GetPessoa(int PessoaId)
+        {
+            return await _dbConnection.Table<Pessoa>().FirstOrDefaultAsync(x => x.Id == PessoaId);
+        }
+
+        public async Task<List<Pessoa>> GetPessoas()
+        {
+            return await _dbConnection.Table<Pessoa>().ToListAsync();
+        }
+
+        public async Task<int> UpdatePessoa(Pessoa pessoa)
+        {
+            return await _dbConnection.UpdateAsync(pessoa);
+        }
+    }
+}
