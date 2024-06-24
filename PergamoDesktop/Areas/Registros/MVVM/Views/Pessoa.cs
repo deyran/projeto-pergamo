d351ebb32983f7dc5ed5963c2ef165f0756fccf9
Deyvid Rannyere <deyran@gmail.com>
2024-06-24 19:08:02 -0300

    Classe Pessoa

---
+++ PergamoDesktop/Areas/Registros/MVVM/Views/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace PergamoDesktop.Areas.Registros.MVVM.Views
+{
+    [Table("REG_PESSOAS")]
+    public class Pessoa
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100), NotNull]
+        public string? Name { get; set; }
+    }
+}
